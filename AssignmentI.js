28f36f3d06f851d1445cf89bab610d7c2a0d5fa4
AAHtet <ayeaye.htet10@gmail.com>
2021-12-26 22:05:12 +0630

    TotalAssignmet=3

---
+++ AssignmentI.js
@@ -0,0 +1,27 @@
+let dataInfo = [
+    mgmgProfile= {
+      name: "Mg Mg",
+      age: 22,
+      job: "Teacher",
+      married: false,
+      education: ["M.Sc"],
+    },
+    aungaungProfile= {
+      name: "Aung Aung",
+      age: 24,
+      job: "Web Design",
+      married: false,
+      education: ["B.Sc"],
+    },
+    myamyaProfile= {
+      name: "Mya Mya",
+      age: 25,
+      job: "Doctor",
+      married: true,
+      education: ["Ph.D"],
+    },
+  ]
+  for(let i=0;i<=dataInfo.length;i++){
+  console.log(`Namee is ${dataInfo[i].name}\nAge is ${dataInfo[i].age}\nJob is ${dataInfo[i].job}\nMarried is ${dataInfo[i].married}\nEducation is ${dataInfo[i].education}\n`)};
+  
+ 
---
+++ AssignmentII&III.js
@@ -0,0 +1,9 @@
+console.log("hellow world")
+let language= ["HTML", "CSS", "JS", "Bootstrap", "TailwindCSS", "Vue", "Angular", "PHP", "Mysql"]
+console.log(language.map(l=>console.log(l)))
+
+let order=1
+while(order<=100){
+    console.log("OrderCount Is"+order)
+    order++
+}
